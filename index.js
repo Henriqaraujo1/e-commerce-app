8973e20cbcffdd080d2fefe49701b034ce83c49e
Henrique Silva <39538139+Henriqaraujo1@users.noreply.github.com>
2022-09-28 00:05:55 -0300

    criação de banco e alguns arquivos do servidor

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { response } = require("express");
 const app = express();
-const PORT = 4001
+const { PORT } = require('./src/configs/config');
 
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (req, res) => {
+    response.json({info: 'E-commerce with Express and Postgres'})
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+startServer();
