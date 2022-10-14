602a33da3b4deb0d3a30bce33cc490ee982c145a
Henrique Silva <39538139+Henriqaraujo1@users.noreply.github.com>
2022-10-14 00:07:12 -0300

    passport login concluido em 90% e crud do path product criado

---
+++ src/routes/products.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router();
+
+const ProductsServices = require("../services/ProductService");
+const ProductServiceInstance = new ProductsServices();
+
+module.exports = (app) => {
+    
+}
--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,74 +1,92 @@
 const client = require("../db/dbConfig");
 const bcrypt = require("bcrypt");
-const createError = require("http-errors")
+const createError = require("http-errors");
 
 const EncryptUtil = require("../utils/bcrypt");
+const { createReadStream } = require("fs");
 const EncryptUtilInstance = new EncryptUtil();
 
 const emailExists = async (email) => {
+  const values = email;
   try {
     const data = await client.query("SELECT * FROM users WHERE email=$1", [
-      email,
+      values,
     ]);
-    if (data.rowCount == 0) {
-      return false;
+    if (data.rows?.length) {
+      return data.rows[0];
+    } else {
+      return createError(400, "Bad Request");
     }
-    return data.rows[0];
   } catch (err) {
-    throw createError(500, "erro Interno")
+    throw createError(500, "erro Interno");
   }
 };
 
 const createUser = async (email, password) => {
   const encryptPassword = await EncryptUtilInstance.encrypt(password);
+
   const data = await client.query(
     "INSERT INTO users(email, password) VALUES ($1, $2)",
-    [email, encryptPassword]
-  );
-
-  if (data.rowCount == 0) {
-    return false;
-  }
-  return data.rows[0];
-};
-
-const matchPassword = async (email, password) => {
-  const userPassword = await client.query(
-    "SELECT password FROM users WHERE email= $1",
-    [email]
-  );
-  if (userPassword.rowCount == 0) {
-    return false;
-  }
-
-  const comparePassword = userPassword.rows[0].password;
-
-  const match = await bcrypt.compare(password, comparePassword);
-  // console.log(match)
-  return match;
-};
-
-const getUserById = (req, res) => {
-  const id = parseInt(req.params.id);
-  client.query(
-    `SELECT * FROM users WHERE id_user = ${id}`,
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      if (!results.rows[0]) {
-        res.status(404).json("Usuario não encontrado");
-      } else {
-        res.status(200).json(results.rows);
+    [email, encryptPassword],
+    (err, result) => {
+      try {
+        if (result.rowCount == 0) {
+          return false;
+        }
+        return result.rows[0];
+      } catch (err) {
+        throw new Error(err);
       }
     }
   );
+  return null;
 };
 
-const getAllUsers = (req, res) => {
-  client.query("SELECT * FROM users ORDER BY id_user ASC", (error, results) => {
-    if (error) {
-      throw error;
+const matchPassword = async (email, password) => {
+  try {
+    const userPassword = await client.query(
+      "SELECT password FROM users WHERE email= $1",
+      [email]
+    );
+    if (userPassword.rowCount == 0) {
+      return false;
+    }
+
+    const comparePassword = userPassword.rows[0].password;
+
+    const match = await bcrypt.compare(password, comparePassword);
+    // console.log(match)
+    return match;
+  } catch (err) {
+    throw createError(500, err);
+  }
+};
+
+const getUserById = (id) => {
+  try {
+    const value = parseInt(id);
+    client.query(
+      `SELECT * FROM users WHERE id_user = ${value}`,
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        if (results.rows?.length) {
+          return results.rows[0];
+        }
+      }
+    );
+
+    return null;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  client.query("SELECT * FROM users ORDER BY id_user ASC", (err, results) => {
+    if (err) {
+      throw err;
     } else {
       res.status(201).json(results.rows);
     }
