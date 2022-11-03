21438c622c4988371327c1ff690025cfae40303b
Henrique <henriqaraujo1@gmail.com>
2022-11-03 17:27:41 -0300

    primeiro teste criado

--- src/test/userTest.js
+++ src/test/userTest.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = "test";
 
-let UsersService = require("../services/UsersService");
+let UsersRoute = require("../routes/users");
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
@@ -9,7 +9,16 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('/GET email', () => {
-    it('Buscar um email ')
-})
-
+describe("/GET email", () => {
+  it("Buscar um email no Banco", (done) => {
+    chai
+      .request(server)
+      .get("/users/allusers")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(0);
+        done();
+      });
+  });
+});
